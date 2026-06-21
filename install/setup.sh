177f78e268021bd28d0d0bed90704d80e50cc9cc
Akut-Luna <felixpaulmeyer@gmail.com>
2026-06-21 09:55:20 +0200

    moved config to install folder

--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Configuration
+# --------------------------------------------- data ----------------------------------------------
 SOURCE="install/example_data"
 DEST="main/data"
 
@@ -17,5 +17,25 @@ else
   echo "Creating $DEST from $SOURCE..."
   mkdir -p "main"
   cp -r "$SOURCE" "$DEST"
+  echo "Setup complete."
+fi
+
+# -------------------------------------------- config ---------------------------------------------
+SOURCE="install/config_antenna.env.example"
+DEST="main/config/config_antenna.env"
+
+# Check if source exists (using -f for file)
+if [ ! -f "$SOURCE" ]; then
+  echo "Error: Source file $SOURCE does not exist."
+  exit 1
+fi
+
+# Copy data if destination file doesn't exist
+if [ -f "$DEST" ]; then
+  echo "File $DEST already exists. Skipping copy."
+else
+  echo "Creating $DEST from $SOURCE..."
+  mkdir -p "main/config"
+  cp "$SOURCE" "$DEST"
   echo "Setup complete."
 fi
