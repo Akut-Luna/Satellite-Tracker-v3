494149b90a80b2483a875bb54094c9a34948b4e5
Akut-Luna <felixpaulmeyer@gmail.com>
2026-06-30 14:16:40 +0200

    clean up

--- satellite_tracker.py
+++ satellite_tracker.py
@@ -6,7 +6,4 @@ if __name__ == '__main__':
     script_path = os.path.join(os.path.dirname(__file__), 'main', 'src', 'main.py')
     subprocess.run([sys.executable, script_path], check=True)
 
-# TODO: documentaion in PDF -> 
-#   Part 1 patchnotes, 
-# TODO: note in v1
 # TODO: clean up signals
